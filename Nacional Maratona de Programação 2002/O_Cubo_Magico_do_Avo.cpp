9a74c6f93d18228c85da17d82708280b4f82c2b1
Kenzo <79611160+Kenzo-Sugai@users.noreply.github.com>
2023-04-27 11:49:07 -0300

    Create O_Cubo_Magico_do_Avo.cpp

---
+++ Nacional Maratona de Programação 2002/O_Cubo_Magico_do_Avo.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void inputCube(vector<vector<vector<char>>> &cube){
+
+    char c;
+
+    for(int i = 0; i < 6; i++) cube[i].resize(3);
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+
+            cin >> c;
+            cube[4][i].push_back(c);
+
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+
+        for(int k = 0; k < 4; k++){
+
+            for(int j = 0; j < 3; j++){
+                
+                cin >> c;
+                cube[k][i].push_back(c);
+
+            }
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+
+            cin >> c;
+            cube[5][i].push_back(c);
+
+        }
+    }
+
+}
+
+void rotateCube(vector<vector<vector<char>>> &cube, int r){
+
+    vector<vector<char>> M(3);
+
+    for(int i = 0; i < 3; i++) M[i].resize(3, ' ');
+
+    if(r < 0){
+
+        r = (r * -1) - 1;
+
+        for(int j = 2, k = 0; j >= 0; j--, k++){
+            for(int i = 0; i < 3; i++){
+                M[k][i] = cube[r][i][j];
+            }
+        }
+
+        cube[r] = M;
+
+    }
+    else{
+
+    }
+
+}
+
+int main(){
+
+    int T;
+
+    cin >> T;
+
+    while(T--){
+
+        vector<vector<vector<char>>> cubo(6);
+
+        inputCube(cubo);
+
+        int r;
+        vector<int> rot;
+
+        while(cin >> r && r) rot.push_back(r);
+        
+        for(int k = 0; k < 6; k++){
+
+            for(int i = 0; i < 3; i++){
+
+                for(int j = 0; j < 3; j++){
+
+                    cout << cubo[k][i][j];
+
+                }
+                cout << endl;
+
+            }
+            cout << endl;
+
+        }
+
+        rotateCube(cubo, -5);
+
+        for(int k = 0; k < 6; k++){
+
+            for(int i = 0; i < 3; i++){
+
+                for(int j = 0; j < 3; j++){
+
+                    cout << cubo[k][i][j];
+
+                }
+                cout << endl;
+
+            }
+            cout << endl;
+
+        }
+
+    }
+
+
+}
